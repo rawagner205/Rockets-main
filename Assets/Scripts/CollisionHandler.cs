7d3c8ef4b628c9569d82faaf256ddfa5b31c6b81
rawagner205 <143737662+rawagner205@users.noreply.github.com>
2025-11-05 12:19:37 -0800

    Finished game alterations
    Implemented score UI, improved collision handling, restart function/end screen.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -15,23 +15,44 @@ public class CollisionHandler : MonoBehaviour
 
     [SerializeField] UIDocument uiDocument;
     private Label healthLabel;
-    
+    private Label scoreLabel;
+    private Label winLabel;
+    private Button restartButton;
+    private Label gameOverLabel;
+
     AudioSource audioSource;
 
     bool isControllable = true;
     bool isCollidable = true;
 
+    //the total score of the overall game
+    static int totalScore = 0;
+    ////the temporary score of each individual level
+    int currentScore = 0;
+
     private void Start() 
     {
         audioSource = GetComponent<AudioSource>();
+
         healthLabel = uiDocument.rootVisualElement.Q<Label>("HealthLabel");
-        healthAmount += 1;
+        scoreLabel = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        winLabel = uiDocument.rootVisualElement.Q<Label>("WinLabel");
+        gameOverLabel = uiDocument.rootVisualElement.Q<Label>("GameOverLabel");
+
+        winLabel.style.display = DisplayStyle.None;
+        gameOverLabel.style.display = DisplayStyle.None;
+
+        restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
+
+        restartButton.clicked += RestartGame;
+
     }
     
     private void Update() 
     {
         RespondToDebugKeys();
         healthLabel.text = " ❤" + healthAmount;
+        scoreLabel.text = " ★" + (totalScore + currentScore);
     }
 
     void RespondToDebugKeys()
@@ -46,25 +67,29 @@ public class CollisionHandler : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision other) 
+    private void OnCollisionEnter(Collision other)
     {
         if (!isControllable || !isCollidable) { return; }
 
         string objTag = other.gameObject.tag;
-        
+
         if (objTag == "Friendly")
         {
             Debug.Log("Everything is looking good!");
         }
-        if (objTag == "Finish")
+        else if (objTag == "Finish")
         {
             StartSuccessSequence();
         }
+        else if (objTag == "Collectible")
+        {
+            AddScore(other);
+        }
+
+        //if collision occurs with obstacle or part of environment
         else
         {
-            healthAmount -= 1;
-            healthLabel.text = " ❤" + healthAmount;
-            healthLabel.style.display = DisplayStyle.Flex;
+            TakeDamage();
             if (healthAmount == 0)
             {
                 StartCrashSequence();
@@ -79,6 +104,8 @@ public class CollisionHandler : MonoBehaviour
         audioSource.PlayOneShot(successSFX);
         successParticles.Play();
         GetComponent<Movement>().enabled = false;
+        winLabel.style.display = DisplayStyle.Flex;
+
         Invoke("LoadNextLevel", levelLoadDelay);
     }
 
@@ -92,23 +119,58 @@ public class CollisionHandler : MonoBehaviour
         Invoke("ReloadLevel", levelLoadDelay);
     }
 
+    void TakeDamage()
+    {
+        healthAmount -= 1;
+        healthLabel.text = " ❤" + healthAmount;
+        healthLabel.style.display = DisplayStyle.Flex;
+    }
+
     void LoadNextLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         int nextScene = currentScene + 1;
-        
+        winLabel.style.display = DisplayStyle.None;
+
+        //if current level is last level, display Game Over screen
         if (nextScene == SceneManager.sceneCountInBuildSettings)
         {
-            nextScene = 0;
+            gameOverLabel.style.display = DisplayStyle.Flex;
+            restartButton.text = "Play Again";
+            restartButton.style.display = DisplayStyle.Flex;
         }
-        
-        SceneManager.LoadScene(nextScene);
+
+        //if current level is NOT last, load next level
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+            totalScore += currentScore;
+            currentScore = 0;
+        }
     }
 
+    //reloads current level - resets health and current level score
     void ReloadLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
+        currentScore = 0;
+    }
+
+    //restarts entire game, sending player back to first level - resets health, current level score, and total score
+    void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+        currentScore = 0;
+        totalScore = 0;
+    }
+
+    void AddScore(Collision other)
+    {
+        currentScore += 1;
+        scoreLabel.text = " ★" + (totalScore + currentScore);
+        scoreLabel.style.display = DisplayStyle.Flex;
+        Destroy(other.gameObject);
     }
 
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
-{   
+{
     [SerializeField] InputAction thrust;
     [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
@@ -16,26 +17,37 @@ public class Movement : MonoBehaviour
     [SerializeField] UIDocument uiDocument;
     Button resumeButton;
     Button restartButton;
+    Button startButton;
 
     Rigidbody rb;
     AudioSource audioSource;
 
+    //for tracking game state
     bool isPaused = false;
+    bool isStarted = false;
 
-    private void Start() 
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+
         resumeButton = uiDocument.rootVisualElement.Q<Button>("ResumeButton");
         restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
+        startButton = uiDocument.rootVisualElement.Q<Button>("StartButton");
 
         resumeButton.style.display = DisplayStyle.None;
-        restartButton.style.display = DisplayStyle.None;  
+        restartButton.style.display = DisplayStyle.None;
+        startButton.style.display = DisplayStyle.None;
+
+        resumeButton.clicked += Unpause;
+        restartButton.clicked += RestartGame;
+        startButton.clicked += StartGame;
+
     }
 
-    private void OnEnable() 
+    private void OnEnable()
     {
-        thrust.Enable(); 
+        thrust.Enable();
         rotation.Enable();
     }
 
@@ -44,17 +56,25 @@ public class Movement : MonoBehaviour
         ProcessThrust();
         ProcessRotation();
         CheckForPause();
+
+        //if game has been paused by user
         if (isPaused == true)
         {
+            Pause();
+        }
+        //if game has not started yet 
+        else if (isStarted == false)
+        {
+            startButton.style.display = DisplayStyle.Flex;
             Time.timeScale = 0;
-            resumeButton.style.display = DisplayStyle.Flex;
-            restartButton.style.display = DisplayStyle.Flex;
+        }
+        else if (isStarted == true)
+        {
+            StartGame();
         }
         else
         {
-            Time.timeScale = 1;
-            resumeButton.style.display = DisplayStyle.None;
-            restartButton.style.display = DisplayStyle.None;
+            Unpause();
         }
     }
 
@@ -92,11 +112,11 @@ public class Movement : MonoBehaviour
     private void ProcessRotation()
     {
         float rotationInput = rotation.ReadValue<float>();
-        if(rotationInput < 0)
+        if (rotationInput < 0)
         {
             RotateRight();
         }
-        else if(rotationInput > 0)
+        else if (rotationInput > 0)
         {
             RotateLeft();
         }
@@ -145,5 +165,38 @@ public class Movement : MonoBehaviour
         {
             isPaused = true;
         }
+    }
+
+    //restarts entire game, sending player back to first level - resets health, current level score, and total score
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+        Unpause();
+    }
+
+    //stop game in entirety, bring up pause menu
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        resumeButton.style.display = DisplayStyle.Flex;
+        restartButton.style.display = DisplayStyle.Flex;
+    }
+
+    //restart game from pause screen
+    private void Unpause()
+    {
+        Time.timeScale = 1;
+        resumeButton.style.display = DisplayStyle.None;
+        restartButton.style.display = DisplayStyle.None;
+        startButton.style.display = DisplayStyle.None;
+        isPaused = false;
+    }
+
+    //start game from start screen
+    private void StartGame()
+    {
+        Time.timeScale = 1;
+        startButton.style.display = DisplayStyle.None;
+        isStarted = true;
     }
 }
